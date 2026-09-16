aa86c797baa9168882cdacdc22f7e92aed51bc06
Mantesh Swami <swamimantesh215@gmail.com>
2026-09-16 21:25:59 +0530

    search agent project added

--- Projects/Search_Weather_Agent/agent.py
+++ Projects/Search_Weather_Agent/agent.py
@@ -5,12 +5,38 @@ import os
 from langchain.agents import create_agent
 from langchain_groq import ChatGroq
 from tools import All_TOOLS
+from langgraph.checkpoint.memory import InMemorySaver
 
 MODEL = os.getenv("MODEL")
 def get_agent():
-    
+    "Get Agent with google search and weather search abilities"
     return create_agent(
         model=ChatGroq(model=MODEL),
         tools=All_TOOLS,
-        
+        system_prompt=(
+            "Ypu are a research assistent with google search and weather tools.\n"
+            "Use 'google_search' for quick search on google"
+            "for deep search that may take several minutes."
+            "if user is looking for weather detatils like temprature, humidity or any other detail then call the weather tool to get the live real time weather data."
+        ),
+        checkpointer=InMemorySaver()
     )
+    
+    
+# agent = get_agent()
+# while True:
+#     query=input("User: ")
+#     if query == "exit":
+#         break
+    
+#     res = agent.invoke(
+#         {"messages":[
+#             {"role":"user",
+#              "content":query
+#             }
+#         ]}
+#     )
+    
+#     ans = res["messages"][-1].content
+#     print("AI: ", ans)
+    
--- Projects/Search_Weather_Agent/tools.py
+++ Projects/Search_Weather_Agent/tools.py
@@ -1,6 +1,7 @@
 from langchain_nimble import NimbleSearchTool
 from langchain.tools import tool
-
+import os
+import requests
 
 search_tool= NimbleSearchTool(
     k=5,
@@ -28,6 +29,10 @@ def weather_tool(city:str):
        
     retuen: weather data from the api respoance
     """
-    return f"The Current Temp in {city} is 23.C"
+    API_URL = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.getenv("WEATHER_API_KEY")}"
+    res = requests.get(API_URL)
+    if res.status_code == 200:
+        return res.json()
+    return "Unable to find details for the {city}"
 
 All_TOOLS = [google_search, weather_tool]
